70da10faf25a8fd52148346a5b8d10f8b7804381
Debakor10 <debakor10@gmail.com>
2021-05-30 20:40:14 +0600

    link fix

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import './Navbar.css';
 import logo from '../../images/logo.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-menu">
             <div class="container">
-                <a class="navbar-brand" href="#home">
+                <Link to="/home">
                     <img className="h-25 w-25" src={logo} alt="" />
-                </a>
+                </Link>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active px-md-4 text-white" aria-current="page" href="#home">HOME</a>
+                            <Link class="nav-link active px-md-4 text-white" aria-current="page" to="/">HOME</Link>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link px-md-4 text-white" href="#about">ABOUT</a>
